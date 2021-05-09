1c42e608c229817c019d8757ea14324a30b9a514
Sam Matthews <sammatthewsnz@gmail.com>
2021-05-10 08:30:50 +1200

    Added function to calculate year performance

---
+++ load/ret_year.sql
@@ -0,0 +1,206 @@
+CREATE OR REPLACE FUNCTION ret_year(
+    dur_type IN VARCHAR, 
+    stocks_to_choose IN INTEGER) RETURNS VOID AS $$
+
+/*
+
+	analyze_type is one of the characterisics we use. Currently weighted_alpha or perc_chg_3mth.
+	dur_type is changing the list of stocks, daily or weekly.
+*/
+
+
+DECLARE
+
+  ref   RECORD;
+  date_start DATE ;
+  date_stop  DATE := '05-04-2099';
+
+  day_of_week         INTEGER;
+  fortnight_counter   INTEGER;
+  start_of_month      BOOLEAN;
+  start_of_two_month  BOOLEAN;
+  start_of_quarter    BOOLEAN;
+
+  loop_counter INTEGER;
+
+
+BEGIN
+  -- initialize.
+  loop_counter := 0;
+
+  raise notice 'Duration Type: %', dur_type;
+  raise notice 'Duration Period: %', stocks_to_choose;
+  
+  -- date_start := (current_date - interval '3 month');
+  SELECT start_date INTO date_start FROM lkp_start_date;
+  
+  DELETE FROM summary 
+  WHERE 1=1
+    AND ret_strategy = 'year' 
+    AND ret_type = dur_type 
+    AND ret_period::integer = stocks_to_choose;
+
+  FOR ref IN SELECT * FROM lkp_dates WHERE prev_date BETWEEN date_start AND date_stop ORDER BY 1 LOOP
+
+    loop_counter := loop_counter + 1;
+
+    -- Assume the first record is the first day of the month and therefore we collect data.
+    IF (loop_counter = 1) THEN 
+      start_of_month := 'true';
+    ELSE
+      start_of_month := 'false';
+    END IF;
+
+    IF (EXTRACT(MONTH FROM ref.data_date)) > (EXTRACT(MONTH FROM ref.prev_date)) OR 
+       (EXTRACT(YEAR FROM ref.data_date))  > (EXTRACT(YEAR FROM ref.prev_date)) 
+    THEN
+      start_of_month := 'true';
+    ELSE 
+      start_of_month := 'false';
+    END IF;
+
+    IF ((EXTRACT(MONTH FROM ref.data_date) IN (2,4,6,8,10,12)) AND start_of_month = 'true')
+    THEN
+      start_of_two_month := 'true';
+    ELSE
+      start_of_two_month := 'false';
+    END IF;
+
+    IF ((EXTRACT(MONTH FROM ref.data_date) IN (1,4,7,10)) AND start_of_month = 'true')
+    THEN
+      start_of_quarter := 'true';
+    ELSE
+      start_of_quarter := 'false';
+    END IF;
+
+    raise notice '------------------';
+    raise notice 'Current date: %', ref.data_date;
+    raise notice 'Previous date: %', ref.prev_date;
+    raise notice 'Start of month: %', start_of_month;
+
+
+    -- Find day_of_week.
+    day_of_week = date_part('dow', ref.prev_date);
+
+    -- IF Quarter Analysis is being run.
+    IF (dur_type = 'quarter' AND start_of_quarter)
+    THEN
+
+      raise notice 'Running Quarter analysis';
+      
+      TRUNCATE TABLE tmp_stocks_to_invest;
+
+      INSERT INTO tmp_stocks_to_invest 
+        SELECT symbol 
+        FROM barchart_data 
+        WHERE 1=1
+              AND data_date = ref.prev_date
+      ORDER BY perc_chg_year::decimal DESC LIMIT stocks_to_choose;
+
+    END IF;
+
+    -- IF 2Month Analysis is being run.
+    IF (dur_type = '2month' AND start_of_two_month)
+    THEN
+
+      raise notice 'Running 2xMonth analysis';
+      
+      TRUNCATE TABLE tmp_stocks_to_invest;
+
+      INSERT INTO tmp_stocks_to_invest 
+        SELECT symbol 
+        FROM barchart_data 
+        WHERE 1=1
+              AND data_date = ref.prev_date
+      ORDER BY perc_chg_year::decimal DESC LIMIT stocks_to_choose;
+
+    END IF;
+
+    -- IF Month Analysis is being run.
+    IF (dur_type = 'month' AND start_of_month = 'true') THEN
+      raise notice 'Running Month analysis';
+      
+      TRUNCATE TABLE tmp_stocks_to_invest;
+
+      INSERT INTO tmp_stocks_to_invest 
+        SELECT symbol 
+        FROM barchart_data 
+        WHERE 1=1
+              AND data_date = ref.prev_date
+      ORDER BY perc_chg_year::decimal DESC LIMIT stocks_to_choose;
+
+    END IF;
+
+    -- IF Week Analysis is being run.
+    IF (dur_type = 'week' AND day_of_week = 5) THEN
+    
+      raise notice 'Updating stocks for the week.';
+    
+      TRUNCATE TABLE tmp_stocks_to_invest;
+
+      INSERT INTO tmp_stocks_to_invest 
+        SELECT symbol 
+        FROM barchart_data 
+        WHERE 1=1
+              AND data_date = ref.prev_date
+      ORDER BY perc_chg_year::decimal DESC LIMIT stocks_to_choose;
+
+    END IF;
+
+
+    -- IF DAY Analysis is being run.
+    IF (dur_type = 'day') THEN
+
+      -- raise notice 'Updating stocks for the day.';
+  
+      TRUNCATE TABLE tmp_stocks_to_invest;
+
+      INSERT INTO tmp_stocks_to_invest 
+        SELECT symbol 
+        FROM barchart_data 
+        WHERE 1=1
+              AND data_date = ref.prev_date
+      ORDER BY perc_chg_year::decimal DESC LIMIT stocks_to_choose;
+
+    END IF;
+  
+  -- Insert barchart data into return table.
+    -- We should be able to query this table to determine over performance of daily changes.
+  -- raise notice 'Loading data into: %', ref.data_date;
+
+    INSERT INTO summary  
+    SELECT b.symbol, b.data_date, 'year', dur_type, stocks_to_choose, b.perc_change_daily 
+    FROM barchart_data b 
+    WHERE 1=1
+      AND b.data_date = ref.data_date 
+      AND b.symbol IN (SELECT symbol FROM tmp_stocks_to_invest);
+
+  -- raise notice '==============';
+
+  END LOOP;
+
+  -- Now we have loaded the data we need into the summary table, we now just need to load this data into the average
+  -- table.
+
+  RAISE NOTICE 'Migrating to summary_return_average';
+
+  DELETE FROM summary_return_average 
+  WHERE 1=1
+    AND ret_strategy = 'year' 
+    AND ret_type = dur_type 
+    AND ret_period::integer = stocks_to_choose;
+
+  INSERT INTO summary_return_average
+  SELECT curr_date, ret_strategy, ret_type, ret_period, ROUND(AVG(chg_1d::decimal),4)
+  FROM summary
+  WHERE 1=1
+    AND ret_strategy = 'year'
+    AND ret_type = dur_type
+    AND ret_period::integer = stocks_to_choose
+  GROUP BY curr_date, ret_strategy, ret_type, ret_period;
+
+  PERFORM FROM calculate_running_total('year', dur_type, stocks_to_choose);
+
+END;
+
+$$ LANGUAGE plpgsql;
